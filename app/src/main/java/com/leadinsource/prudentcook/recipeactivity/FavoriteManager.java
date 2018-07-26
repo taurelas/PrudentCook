3d8af9a29ff88c9562a70561a84cbdbec43209a3
Matt <mbeimcik@gmail.com>
2018-07-26 06:43:16 +0200

    fab button works

---
+++ app/src/main/java/com/leadinsource/prudentcook/recipeactivity/FavoriteManager.java
@@ -0,0 +1,49 @@
+package com.leadinsource.prudentcook.recipeactivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import java.util.Map;
+
+public class FavoriteManager {
+
+    public static void saveRecipe(Context context, String recipeName, String ingredients) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(recipeName, ingredients);
+        editor.apply();
+    }
+
+    public static void removeRecipe(Context context, String recipeName) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.remove(recipeName);
+        editor.apply();
+    }
+
+    public static void switchRecipe(Context context, String recipeName, String ingredients) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        if(sharedPreferences.getString(recipeName, "").equals("")) {
+            editor.putString(recipeName, ingredients);
+        } else {
+            editor.remove(recipeName);
+        }
+        editor.apply();
+    }
+
+    public static boolean isFavorite(Context context, String recipeName) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+
+        // returns false if default value (string not found), true otherwise
+        return !sharedPreferences.getString(recipeName, "").equals("");
+
+    }
+
+    public static Map<String, ?> getFavorites(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getAll();
+    }
+}
--- app/src/main/java/com/leadinsource/prudentcook/recipeactivity/RecipeActivity.java
+++ app/src/main/java/com/leadinsource/prudentcook/recipeactivity/RecipeActivity.java
@@ -1,8 +1,11 @@
 package com.leadinsource.prudentcook.recipeactivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
@@ -11,9 +14,16 @@ import com.google.android.gms.ads.MobileAds;
 import com.leadinsource.prudentcook.R;
 import com.leadinsource.prudentcook.mainactivity.MainActivity;
 
+/**
+ * App photos unless otherwise stated are CC0 licensed (no attribution required)
+ */
 public class RecipeActivity extends AppCompatActivity {
 
     private AdView adView;
+    private AdRequest adRequest;
+    private String recipeName;
+    private String ingredients;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +37,8 @@ public class RecipeActivity extends AppCompatActivity {
             return;
         }
 
-        String recipeName = intent.getStringExtra(MainActivity.EXTRA_RECIPE_NAME);
-        String ingredients = intent.getStringExtra(MainActivity.EXTRA_INGREDIENTS);
+        recipeName = intent.getStringExtra(MainActivity.EXTRA_RECIPE_NAME);
+        ingredients = intent.getStringExtra(MainActivity.EXTRA_INGREDIENTS);
         String steps = intent.getStringExtra(MainActivity.EXTRA_STEPS);
 
         setTitle(recipeName);
@@ -39,12 +49,45 @@ public class RecipeActivity extends AppCompatActivity {
 
         tvIngredients.setText(ingredients);
         tvSteps.setText(steps);
+        fab = findViewById(R.id.fab);
 
+        new AsyncTask<Void, Void, Void>() {
 
-        MobileAds.initialize(this, "ca-app-pub-1785118724408316~9524578956");
-        //...
-        adView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        adView.loadAd(adRequest);
+            @Override
+            protected Void doInBackground(Void... voids) {
+                MobileAds.initialize(RecipeActivity.this, "ca-app-pub-1785118724408316~9524578956");
+                //...
+                adView = findViewById(R.id.adView);
+                adRequest = new AdRequest.Builder().build();
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                adView.loadAd(adRequest);
+            }
+        }.execute();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateFabButton();
+
+    }
+
+    public void onFabClick(View view) {
+        FavoriteManager.switchRecipe(this, recipeName, ingredients);
+        updateFabButton();
+    }
+
+    private void updateFabButton() {
+
+        if(FavoriteManager.isFavorite(this, recipeName)) {
+            fab.setImageResource(R.drawable.ic_favorite_white_24dp);
+        } else {
+            fab.setImageResource(R.drawable.ic_favorite_border_white_24dp);
+        }
     }
 }
